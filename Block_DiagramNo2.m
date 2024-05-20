4bbe523bf1fd40f0e14d664dac7dc9e237389163
stnll <157781411+stnll@users.noreply.github.com>
2024-05-21 00:16:25 +0800

    Update Block_DiagramNo2.m

--- Block_DiagramNo2.m
+++ Block_DiagramNo2.m
@@ -1,11 +1,10 @@
-% Clear
+%Clear
 clear
 clc
 close all
 
-%Before Reduce Block Diagram
-%%Define G1, G2, G3,G4, H1, H2, and H3
-
+%%Number 2
+%% DEFINE G1, G2, G3, H1, H2 AND H3
 G1_num = [0 1]
 G1_den = [1 0 0]
 
@@ -26,16 +25,6 @@ H2_den = [1 -1]
 
 H3_num = [0 1]
 H3_den = [1 -2]
-
-%AfterReduce Block Diagram
-
-%Block Diagram Algebra
-%G_1 = 1/s^2
-%G_2 = 1/(s + 1)
-%G_3 = 1/s
-%H_1 = 1/s
-%H_2 = 1/(s - 1)
-%H_3 = 1/(s - 2)
 
 G_num = [2 -6 4 0 0 0 0 0]
 G_den = [4 -8 -4 14 -8 -2 2 6 4 0 0 0 0]
