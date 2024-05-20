ffdda3284f0388ede2ccf7e3bf9c5f93ab7c995f
stnll <157781411+stnll@users.noreply.github.com>
2024-05-21 00:17:02 +0800

    Update Block_DiagramNo1.m

--- Block_DiagramNo1.m
+++ Block_DiagramNo1.m
@@ -29,7 +29,6 @@ H3_num = [0 1];
 H3_den = [1 -2];
 H3 = tf(H3_num,H3_den)
 
-%%Reduce Block Diagram for G2 and H2
 GH2_den = conv(G2_den,H2_den);
 GH2_num = conv(G2_num,H2_num);
 GH2= tf(GH2_num, GH2_den)
@@ -39,13 +38,11 @@ T_F2den_add = GH2_den + [0 0 1]
 T_F2den = conv(T_F2den_add, G2_den)
 TF2 = tf(T_F2num,T_F2den)
 
-%%FOR 1/G2 branch
 G2_branchrule_num = [1]
 G2_num_along = [1]
 TF_G2_branch = [G2_branchrule_num / G2] + G2_num_along
 Recip_G2_num = TF_G2_branch
 
-%%Reduce Block Diagram for G3 and H3
 GH3_den = conv(G3_den,H3_den);
 GH3_num = conv(G3_num,H3_num);
 
@@ -54,43 +51,31 @@ T_F3den_add = GH3_den+ GH3_num;
 T_F3den = conv(G3_den,T_F3den_add)
 TF3 = tf(T_F3num,T_F3den)
 
-%%Reduce Block Diagram for G3 and H3, 1/G2 branch
 TF_G3H3recipG2_num = [1 0 -4 0]
 TF_G3H3recipG2_den = T_F3den
 TF_G3H3recipG2 = tf(TF_G3H3recipG2_num, TF_G3H3recipG2_den)
 
-%%Reduce Block Diagram for G1 and G2H2
 TF_G1G2H2_num = conv(G1_num, T_F2num)
 TF_G1G2H2_den= conv(G1_den, T_F2den)
 TF_G1G2H2= tf(TF_G1G2H2_num, TF_G1G2H2_den)
 
-%%Reduce Block Diagram for H1 and G1G2H2
 G1G2H2H1_den = conv(TF_G1G2H2_den, H1_den)
 G1G2H2H1_num = conv(TF_G1G2H2_num, H1_num)
 G1G2H2H1 = tf(G1G2H2H1_num,G1G2H2H1_den)
 
-%%denominator
 TF_G1G2H2H1_den_add = G1G2H2H1_den + TF_G1G2H2_num
 TF_G1G2H2H1_den = conv(TF_G1G2H2H1_den_add,TF_G1G2H2_den)
 TF_G1G2H2H1_num = conv(TF_G1G2H2_num,G1G2H2H1_den)
 TF_G1G2H2H1= tf(TF_G1G2H2H1_num,TF_G1G2H2H1_den)
 
-%%%%for TF_G1G2H2H1=tf(TF_G1G2H2H1_num,TF_G1G2H2H1_den)
-%%% say A_num = TF_G1G2H2H1_num
-%%% say A_den = TF_G1G2H2H1_den
 A_num = [1 1 -1 -1 0 0 0 0 0]
 A_den = [1 2 1 0 1 1 -1 -1 0 0 0 0]
 
-%%%TF_G3H3recipG2 = tf(TF_G3H3recipG2_num, TF_G3H3recipG2_den)
-%%% say B_num = TF_G3H3recipG2_num
-%%% say B_den = TF_G3H3recipG2_den
 B_num = [1 0 -4 0]
 B_den = [1 -2 1 0]
 
-%%since series, 
 TF_reduced_num = conv(A_num,B_num)
 TF_reduced_den = conv(A_den,B_den)
 TF_reduced = tf(TF_reduced_num, TF_reduced_den)
 
-%%STEP RESPONSE
 step(TF_reduced,0:0.1:20)
